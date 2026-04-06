f100af718e2e6a55f1f14bef7edad1560a75e9eb
ecardoso106 <eccardoso05@gmail.com>
2026-04-06 16:17:52 -0500

    addign front end in react, and backend (back end not implemented)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import ExampleComponent from './components/ExampleComponent';
+
+export default function App() {
+  return (
+    <div>
+      <ExampleComponent />
+    </div>
+  );
+}
